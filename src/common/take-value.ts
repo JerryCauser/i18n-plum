3a326299c5cfd28a751183b45c2fff6b1eb3f23b
Artem Abzanov <jerrycauser@gmail.com>
2023-02-28 19:11:30 +0400

    refactor: take value. now it cannot take nullLike map

--- src/common/take-value.ts
+++ src/common/take-value.ts
@@ -1,19 +1,16 @@
 import type { Nullable } from './types.js'
 
 /**
- * @param {Nullable<Map<string, any>>} map
+ * @param {Map<string, any>} map
  * @param {string | string[]} keys
  * @param {string} [separator]
  * @return {Nullable<string | Function>}
  */
 function takeValue (
-  map: Nullable<Map<string, any>>,
+  map: Map<string, any>,
   keys: string | string[],
   separator: string = '.'
 ): Nullable<string | Function> {
-  if (map === null || map === undefined) {
-    return Array.isArray(keys) ? keys.join(separator) : keys
-  }
   if (typeof keys === 'string') keys = keys.split(separator)
   if (!Array.isArray(keys) || keys.length === 0) return undefined
   if (keys.length === 1) return map.get(keys[0])
